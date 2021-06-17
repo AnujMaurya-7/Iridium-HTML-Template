ff9c42fbd67af5cf581c1284cab7ab6ec6ad6adb
anuj <anuj89806@gmail.com>
2021-06-17 23:35:50 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector('.hamburger');
+const nav = document.querySelector('.links');
+
+
+
+
+menu.addEventListener('click', () => {
+nav.classList.toggle('change');
+
+
+
+});
